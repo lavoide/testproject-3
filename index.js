269a0075c2128a15afead94d8cb53ab505c69f3f
lavoide <lavoidde@gmail.com>
2018-06-23 12:12:48 +0300

    23.06

--- index.js
+++ index.js
@@ -6,3 +6,28 @@ menu.onclick = function myFunc() {
     }else
     myMenu.className += " responsive";
 }
+var circle = document.getElementsByClassName("circle");
+for(var i = 0;i<circle.length;i++)
+{
+    circle[i].onclick = function(){
+        var book1 = document.getElementsByClassName("book1");
+        var book2 = document.getElementsByClassName("book2");
+        var book3 = document.getElementsByClassName("book3");
+        var arr = [book1, book2, book3];
+        for(var j = 0;j<circle.length;j++) {
+            circle[j].classList.remove("active");
+        }
+        this.classList.add("active");
+        var index = this.
+        arr[i].classList.remove("none");
+        // if (circle[i].classList.contains("active")) {
+        //     console.log("good")
+        //     for(var j = 0;j<circle.length-1;j++){
+        //         circle[j].classList.remove("active");
+        //     }
+        //     circle[i].classList.add("active");
+        // } else {console.log("error");
+
+    }
+
+}
