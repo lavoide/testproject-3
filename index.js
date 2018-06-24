25974466724ae18a19cb725f7c3a81f5d0ebee14
lavoide <lavoidde@gmail.com>
2018-06-24 22:44:53 +0300

    finalizing

--- index.js
+++ index.js
@@ -1,11 +1,55 @@
+document.onload = function () {
+    var success = document.getElementById("success");
+    success.classList.toggle("none");
+    document.getElementById('mail').className="";
+    document.getElementById('number').className="";
+}
 
-menu.onclick = function myFunc() {
+menu.onclick = function(){
+    event.preventDefault();
     var myMenu = document.getElementById('topnav');
     if( myMenu.className === "topnav responsive") {
         myMenu.className = "topnav";
     }else
     myMenu.className += " responsive";
 }
+
+document.getElementById("submitbut").onclick = function(){
+    event.preventDefault();
+    function ValidName() {
+        var re = /^[a-zA-Zа-яА-Я'][a-zA-Zа-яА-Я-' ]+[a-zA-Zа-яА-Я']?$/u;
+        var myName = document.getElementById('firstname').value;
+        var valid = re.test(myName);
+        if (valid){
+            return true;
+        }
+        else{
+            document.getElementById('firstname').classList.add("wrong");
+            document.getElementById("errortext1").classList.remove("none");
+            return false;
+        }
+    }
+    function ValidPhone() {
+        var re = /^\d[\d\(\)\ -]{4,14}\d$/;
+        var myPhone = document.getElementById('number').value;
+        var valid = re.test(myPhone);
+        if (valid){
+            return true;
+        }
+        else{
+            document.getElementById('number').classList.add("wrong");
+            document.getElementById("errortext2").classList.remove("none");
+            return false;
+        }
+    }
+    var x = ValidPhone();
+    var y = ValidName();
+    if(x==1 && y==1) {
+        success.classList.remove("none");
+    }
+}
+
+
 var circle = document.getElementsByClassName("circle");
 for(var i = 0;i<circle.length;i++)
 {
@@ -18,8 +62,7 @@ for(var i = 0;i<circle.length;i++)
             circle[j].classList.remove("active");
         }
         this.classList.add("active");
-        var index = this.
-        arr[i].classList.remove("none");
+        // arr[i].classList.remove("none");
         // if (circle[i].classList.contains("active")) {
         //     console.log("good")
         //     for(var j = 0;j<circle.length-1;j++){
@@ -30,4 +73,33 @@ for(var i = 0;i<circle.length;i++)
 
     }
 
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementsByClassName("rtext");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        captionText[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText[slideIndex-1].style.display = "block";
 }
